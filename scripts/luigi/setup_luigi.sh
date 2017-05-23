869d9aa4b731f2c6349e5671b6c6eb1731138ba4
Eoin O'Keeffe <eokeeffe@gmail.com>
2017-05-23 09:46:20 +0100

    setting up luigi

--- scripts/luigi/setup_luigi.sh
+++ scripts/luigi/setup_luigi.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# install luigi
+pip install luigi
+
+# add environment variables
+cp .bashrc .bashrc.bkup.luigiinstall
+echo export\ PATH="/home/ec2-user/miniconda/bin:\$PATH" >> .bashrc
+echo export\ PYTHONPATH="/home/ec2-user/repos/boiler_deploy/examples/luigi_tasks:\$PYTHONPATH" >> .bashrc
--- scripts/python/setup_python.sh
+++ scripts/python/setup_python.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# change to local home directory and ec2-user
+# change to local home directory and check that we can user the ec2-user
 echo "User:"
 whoami
-sudo su - ec2-user
 echo "user: "
-whoami
+sudo -u ec2-user bash -c "whoami"
 cd ~/
 echo "Current Dir:"
 pwd 
@@ -16,20 +15,30 @@ sudo yum install screen -y
 sudo yum install git-all -y
 
 
+# need to run in subshell so we can use the ec2-user
 
+sudo -u ec2-user bash <<"EOF"
 # Download and install minconda
 wget 'https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh' -O ~/miniconda.sh
 cd ~
 sudo chmod a+rwx miniconda.sh
-sudo -u ec2-user bash $HOME/miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-rm miniconda.sh
-
+~/miniconda.sh -b -p /home/ec2-user/miniconda
 # create and activate the environment
+export PATH="/home/ec2-user/miniconda/bin:$PATH"
+rm miniconda.sh
 conda create -n newenviron --yes python=3.5 pip scipy pandas numpy psycopg2 sphinx pylint
 source activate newenviron
 
-# pull down whatever files you wish to run - add code here
+# pull down whatever files you wish to run - add code here 
+# the following is shown as an example where we
+# wish to run luigi scripts
+
 cd ~
 mkdir repos
 cd repos
+git clone https://github.com/ejokeeffe/boiler_deploy.git
+cd boiler_deploy/scripts/luigi
+sudo chmod a+rwx setup_luigi.sh
+bash setup_luigi.sh
+
+EOF
