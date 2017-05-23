c0a33564612399457115850ea1940324e9af9d0d
Eoin O'Keeffe <eokeeffe@gmail.com>
2017-05-23 10:27:48 +0100

    create demo luigi config

--- scripts/luigi/setup_luigi.sh
+++ scripts/luigi/setup_luigi.sh
@@ -4,6 +4,12 @@
 pip install luigi
 
 # add environment variables
+cd ~
 cp .bashrc .bashrc.bkup.luigiinstall
-echo export\ PATH="/home/ec2-user/miniconda/bin:\$PATH" >> .bashrc
 echo export\ PYTHONPATH="/home/ec2-user/repos/boiler_deploy/examples/luigi_tasks:\$PYTHONPATH" >> .bashrc
+
+#copy the config across
+cp ~/repos/boiler_deploy/scripts/luigi.cfg ~/luigi.cfg
+
+# add the environment variable for the location of this file
+echo export\ LUIGI_CONFIG_PATH="/home/ec2-user/luigi.cfg" >> .bashrc
