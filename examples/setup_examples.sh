dbf0fd93f9d76eed42910c4840a4c84d9d247fd8
Eoin O'Keeffe <eokeeffe@gmail.com>
2017-05-23 10:39:09 +0100

    adding examples for luigi

---
+++ examples/setup_examples.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# get the example file from http://luigi.readthedocs.io/en/stable/example_top_artists.html
+cd ~
+mkdir ~/tasks
+wget https://github.com/spotify/luigi/blob/master/examples/top_artists.py -O ~/tasks/top_artists.py
+
--- scripts/luigi/setup_luigi.sh
+++ scripts/luigi/setup_luigi.sh
@@ -6,10 +6,10 @@ pip install luigi
 # add environment variables
 cd ~
 cp .bashrc .bashrc.bkup.luigiinstall
-echo export\ PYTHONPATH="/home/ec2-user/repos/boiler_deploy/examples/luigi_tasks:\$PYTHONPATH" >> .bashrc
+echo export\ PYTHONPATH="/home/ec2-user/repos/boiler_deploy/examples/luigi_tasks:/home/ec2-user/tasks:\$PYTHONPATH" >> .bashrc
 
 #copy the config across
-cp ~/repos/boiler_deploy/scripts/luigi.cfg ~/luigi.cfg
+cp ~/repos/boiler_deploy/scripts/luigi/luigi.cfg ~/luigi.cfg
 
 # add the environment variable for the location of this file
 echo export\ LUIGI_CONFIG_PATH="/home/ec2-user/luigi.cfg" >> .bashrc
